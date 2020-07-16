77193eb0c36e6e65a42d7446d3db6eb0ea3558f2
leydev <wesley.waaraujo@gmail.com>
2020-07-16 03:07:19 -0300

    configuração da store para guardar lista de personagens]

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,27 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
+import Vuex from 'vuex';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    personagens: []
   },
   mutations: {
+    savePersonganes(state, data) {
+      state.personagens = state.personagens.concat(data)
+    },
+    rmPersonagem(state, index) {
+      state.personagens.splice(index, 1)
+    }
   },
   actions: {
+    setPersonagens({ commit }, data) {
+      commit('savePersonganes', data)
+    },
+    rmPersonagenByIndex({ commit }, index) {
+      commit('rmPersonagem', index)
+    }
   },
   modules: {
   }
